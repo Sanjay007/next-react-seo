b0fe70d1a2d6b1fa56e3561d9c4a64aac4cba935
Sanjay Yadav <yadavsanjay@hotmail.com>
2019-09-01 19:37:05 +0530

    Update product.js

--- pages/product.js
+++ pages/product.js
@@ -15,6 +15,15 @@ class Product extends React.Component {
             <Head>
                 <meta name="title" content={"Peaky Blinder's " + this.getProduct().name} />
                 <meta name="description" content={this.getProduct().description} />
+                    
+                    
+                    <meta  property="og:title" content= {this.getProduct().name} />
+<meta  property="og:type" content="website"/>
+<meta property="og:url" content="http://quicknews360.com/Jharkhand/chief-minister-raghuvar-das-said-all-cooperation-is-necessary-for-malnutrition-free-jhark-01631457790344-2KI0H"/>
+<meta property="og:image"content={this.getProduct().image}  />
+<meta property="og:description" content={this.getProduct().description}  />
+    
+    
             </Head>
             
             <a href="/">go back to home</a>
